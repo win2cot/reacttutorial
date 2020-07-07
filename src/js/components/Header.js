bd62db06f40240a4c70b3a3569c0a31f376cd3c5
win2cot <ishikawa.masayuki.0605@gmail.com>
2020-07-08 03:13:54 +0900

    state props

--- src/js/components/Header.js
+++ src/js/components/Header.js
@@ -2,11 +2,11 @@ import React from "react";
 import Title from "./Header/Title"
 
 export default class Header extends React.Component {
-  render() {
-    return (
-      <header>
-          <Title />
-          </header>
-    );
-  }
+    render() {
+        return (
+            <header>
+                <Title title={this.props.title} />
+            </header>
+        );
+    }
 }
--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -3,10 +3,19 @@ import Header from "./Header"
 import Footer from "./Footer"
 
 export default class Layout extends React.Component {
+    constructor() {
+        super();
+        this.state = {title: "Welcome"};
+    }
     render() {
+        setTimeout(
+            () => { this.setState({title: "Welcome win2cot!"});},
+            2000
+        );
         return (
             <div>
-                <Header />
+                <Header title={this.state.title} />
+                <Header title={"thank you"} />
                 <Footer />
             </div>
         );
