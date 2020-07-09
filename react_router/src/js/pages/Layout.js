2fafa2767df6aa5347afd4344cdd2709c7434757
win2cot <ishikawa.masayuki.0605@gmail.com>
2020-07-10 01:54:36 +0900

    linkbutton

--- react_router/src/js/pages/Layout.js
+++ react_router/src/js/pages/Layout.js
@@ -1,9 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default class Layout extends React.Component {
   render() {
     return (
-      <h1>KillerNews.net</h1>
+        <div>
+            <h1>KillerNews.net</h1>
+            {this.props.children}
+            <Link to="/archives" class="btn btn-danger">archives</Link>
+            <Link to="/settings" class="btn btn-success">settings</Link>
+        </div>
     );
   }
 }
